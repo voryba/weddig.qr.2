347eeea578166de15d420f02ef4d5c8941f96c06
voryba <erasabyr14@gmail.com>
2021-09-18 17:57:50 +0600

    finished

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    // $('.header__list').click(function(event) {
-    //     $('.header__burger,.header__menu').removeClass('active');
-    //     $('body').removeClass('lock');
-    // });
+    $('.header__list').click(function(event) {
+        $('.header__burger,.header__menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
 });
 
 $(window).scroll(function() {     
